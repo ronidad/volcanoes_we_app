5c0cafb10fe3d4b711698da8265ba2c9d418a28f
Ronald Mosomi <ronaldmosomi@gmail.com>
2021-08-27 18:32:36 +0300

    added map object

--- map.py
+++ map.py
@@ -1,2 +1,19 @@
+import folium
+from folium.map import Icon, Popup
+
+
+
 
 office = [-1.2897190818854458, 36.783920015980534]
+home= [-1.3433738618892992, 36.98445237479201]
+map = folium.Map(office, zoom_start=10,tiles= "Stamen Terrain")
+
+#folium.LayerControl().add_to(map) 
+#map.add_child(folium.Marker(location=[33.4, -7.5]), popup="Hi, I am a marker", icon=folium.Icon(color="green"))
+for cordinate in [home, office]:
+    maker_name = [k for k, v in locals().items() if v == cordinate][0] 
+ 
+    folium.Marker( location=cordinate, popup=maker_name, icon=folium.Icon(color='green', icon="cloud") ).add_to(map)
+
+
+map.save('map.html')
